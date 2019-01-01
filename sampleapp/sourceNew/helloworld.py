2d76ea617a17f3d6650c3771812509028440514b
deepashreekurnad <46148891+deepashreekurnad@users.noreply.github.com>
2019-01-01 10:09:07 +0530

    Update helloworld.py

--- sampleapp/sourceNew/helloworld.py
+++ sampleapp/sourceNew/helloworld.py
@@ -3,6 +3,16 @@ import webapp2
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, webapp2!')
+        self.response.write("""<html><title>login page</title><body><form action="/action_page.php">
+  <fieldset>
+    <legend>Personal information:</legend>
+    First name:<br>
+    <input type="text" name="firstname" value="Mickey"><br>
+    Last name:<br>
+    <input type="text" name="lastname" value="Mouse"><br><br>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form></body></html>""")
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
